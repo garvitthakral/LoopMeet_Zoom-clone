1df22af41179dd984c9a6f7a8fc1303c0cbedcec
GARVIT THAKRAL <garvitthakral1001@gmail.com>
2025-06-28 05:50:10 +0530

    Remove unused video call components and simplify VideoCallPage
    Deleted VideoCall, VideoMeet, and VideoZoom components, consolidating video call functionality into VideoCallPage. The VideoCallPage component was replaced with a placeholder, and related routes and imports were updated in App.jsx to reflect these changes. This cleanup removes redundant code and prepares for a new or refactored video call implementation.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/landing/Home'
 import Login from './pages/register/Login'
 import Signin from './pages/register/Signin'
-import GenratingRoom from './pages/roomkey/GenratingRoom'
 import WaitingRoom from './pages/videocall/WaitingRoom'
-import VideoMeet from './pages/videocall/VideoMeet'
-import VideoZoom from './pages/videocall/VideoZoom'
 import VideoCallPage from './pages/videocall/VideoCallPage'
 
 const App = () => {
@@ -19,7 +16,7 @@ const App = () => {
           <Route path='/login' element={<Login />}/>
           <Route path='/register' element={<Signin />}/>
           <Route path='/generate-room-key' element={<WaitingRoom/>}/>
-          {/* <Route path='/waiting-room' element={<WaitingRoom />}/> */}
+          <Route path='/waiting-room' element={<WaitingRoom />}/>
           <Route path='/video-call/:id' element={<VideoCallPage/>}/>
         </Routes>
     </div>
